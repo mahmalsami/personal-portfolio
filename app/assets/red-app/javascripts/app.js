a9cfdfe8da689348a28eb00d7031cc605104749e
Samix <reaverhack@gmail.com>
2014-08-16 20:47:15 +0200

    fix js error

--- app/assets/red-app/javascripts/app.js
+++ app/assets/red-app/javascripts/app.js
@@ -1,12 +1,4 @@
 (function ($, App) { "use strict";
 
-  window.onYouTubePlayerAPIReady = function() {
-    if(!!window.IntrovideoYT){
-      window.IntrovideoYT();
-    }
-    if(!!window.PlaylistYT){
-      window.PlaylistYT();
-    }
-  };
 
 }(window.jQuery, window.App));
