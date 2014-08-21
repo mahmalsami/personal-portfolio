d9c148e4cb1b72c8fd5f06b58093aec165c4d6a4
Samix <reaverhack@gmail.com>
2014-08-22 00:15:04 +0200

    remove console.log

--- app/assets/red-header/javascripts/app.js
+++ app/assets/red-header/javascripts/app.js
@@ -12,38 +12,6 @@
 
 (function ($, App) { "use strict";
 
-  $(function () {
-    $('.red-header').on('click', '.btn-navigation', function(evt){
-      evt.preventDefault();
-      $('#navmenu').toggleClass('open');
-    });
-    $('.red-header .navigation').affix({
-      offset: {
-        top: 105
-      }
-    });
 
-    $('.red-header').on('click touchend', '[data-nav="scroll"]', function (evt) {
-      evt.preventDefault();
-      var target = $( $(this).attr('href') ),
-          topContent = 0,
-          fixedNav = $('#navmenu');
-
-      if(!target.length){
-        return;
-      }
-
-      if(fixedNav.height() > 100) {
-        topContent = $('.red-header').height();
-      } else if(!fixedNav.hasClass('affix')) {
-        topContent = fixedNav.height() * 2;
-      } else {
-        topContent = fixedNav.height();
-      }
-      var offsetTop = target.offset().top - topContent;
-      $('html, body').stop().animate({ scrollTop: offsetTop+'px' }, 400);
-      $('#navmenu').toggleClass('open');
-    });
-  });
 
 }(window.jQuery, window.App));
